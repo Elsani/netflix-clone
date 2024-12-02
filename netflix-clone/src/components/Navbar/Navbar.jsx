224e0799e22e883313aa32ec824a8532c7390656
Elsani <elsan.memeti@hotmail.com>
2024-12-02 14:45:47 +0100

    Netflix-clone

---
+++ netflix-clone/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
+const Navbar = () => {
+  return (
+    <div className="Navbar">
+      <div className="navbar-left">
+        <img src={logo} alt="" />
+      </div>
+      <div className="navbar-right"></div>
+      navbar
+    </div>
+  );
+};
+
+export default Navbar;
