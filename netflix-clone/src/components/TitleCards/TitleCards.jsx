cf38c4aacf2701bdddfa722e24b2d034b83bcc09
Elsani <elsan.memeti@hotmail.com>
2024-12-04 20:51:45 +0100

    usereff/useefffect-cards

--- netflix-clone/src/components/TitleCards/TitleCards.jsx
+++ netflix-clone/src/components/TitleCards/TitleCards.jsx
@@ -15,9 +15,9 @@ const TitleCards = () => {
   }, []);
 
   return (
-    <div className="title-cards" ref={cardsRef}>
+    <div className="title-cards">
       <h2>Popular on Netflix</h2>
-      <div className="card-list">
+      <div className="card-list" ref={cardsRef}>
         {cards_data.map((card, index) => {
           return (
             <div className="card" key={index}>
