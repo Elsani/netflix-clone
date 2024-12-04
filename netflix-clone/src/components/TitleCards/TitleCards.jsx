2d8e35bc479083a0d835ededefbd8e9c9f0a36ac
Elsani <elsan.memeti@hotmail.com>
2024-12-04 16:22:00 +0100

    cardflip-useeffect

--- netflix-clone/src/components/TitleCards/TitleCards.jsx
+++ netflix-clone/src/components/TitleCards/TitleCards.jsx
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./TitleCards.css";
+import cards_data from "../../assets/cards/Cards_data";
 
-function TitleCards() {
-  return <div className="TitleCards"></div>;
-}
+const TitleCards = () => {
+  const cardsRef = useRef();
+
+  const handleWheel = (event) => {
+    event.preventDefault();
+    cardsRef.current.scrollLeft += event.deltaY;
+  };
+
+  useEffect(() => {
+    cardsRef.current.addEventListener("wheel", handleWheel);
+  }, []);
+
+  return (
+    <div className="title-cards" ref={cardsRef}>
+      <h2>Popular on Netflix</h2>
+      <div className="card-list">
+        {cards_data.map((card, index) => {
+          return (
+            <div className="card" key={index}>
+              <img src={card.image} alt="" />
+              <p>{card.name}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default TitleCards;
