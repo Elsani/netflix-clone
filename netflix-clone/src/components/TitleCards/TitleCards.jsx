4edd2b34ce788f7700372a31ffa4e2de914e3924
Elsani <elsan.memeti@hotmail.com>
2024-12-08 12:58:58 +0100

    response_results

--- netflix-clone/src/components/TitleCards/TitleCards.jsx
+++ netflix-clone/src/components/TitleCards/TitleCards.jsx
@@ -2,20 +2,20 @@ import React, { useEffect, useRef, useState } from "react";
 import "./TitleCards.css";
 import cards_data from "../../assets/cards/Cards_data";
 
-const TitleCards = ({title,category}) => {
+const TitleCards = ({ title, category }) => {
+
   const cardsRef = useRef();
 
-    const [apiData,setApiData] = useState([])
+  const [apiData, setApiData] = useState([]);
 
   const options = {
-    method: 'GET',
+    method: "GET",
     headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NDUwYWIwZmVmODAxNjE4M2UyZTVhM2ViMTczMTMxMCIsIm5iZiI6MTczMzY1NzE1MC41MjE5OTk4LCJzdWIiOiI2NzU1ODIzZTQzMTEwOGFhMjNkMTA2MTUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.mmcfIq0HQAmOOFDvMCbezOKSfwcgbnDqf-7CE2JyTlg'
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NDUwYWIwZmVmODAxNjE4M2UyZTVhM2ViMTczMTMxMCIsIm5iZiI6MTczMzY1NzE1MC41MjE5OTk4LCJzdWIiOiI2NzU1ODIzZTQzMTEwOGFhMjNkMTA2MTUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.mmcfIq0HQAmOOFDvMCbezOKSfwcgbnDqf-7CE2JyTlg"
     }
   };
-  
-
 
   const handleWheel = (event) => {
     event.preventDefault();
@@ -24,24 +24,29 @@ const TitleCards = ({title,category}) => {
 
   useEffect(() => {
 
-    fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
-    .then(res => res.json())
-    .then(res => csetApiData(Response.result))
-    .catch(err => console.error(err));
-
+    fetch(
+      "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
+      options
+    )
+      .then((res) => res.json())
+      .then((res) => setApiData(res.results))
+      .catch(err => console.error(err));
 
     cardsRef.current.addEventListener("wheel", handleWheel);
   }, []);
 
   return (
     <div className="title-cards">
-      <h2>{title?title:"Popular on Netflix"}</h2>
+      <h2>{title ? title : "Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
-        {cards_data.map((card, index) => {
+        {apiData.map((card, index) => {
           return (
             <div className="card" key={index}>
-              <img src={card.image} alt="" />
-              <p>{card.name}</p>
+              <img
+                src={`https://image.tmdb.org/t/p/w500` + card.backdrop_path}
+                alt=""
+              />
+              <p>{card.original_title}</p>
             </div>
           );
         })}
