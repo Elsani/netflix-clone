4d1a702e26b0367e0320b6c85270386cac5ef166
Elsani <elsan.memeti@hotmail.com>
2024-12-09 22:42:11 +0100

    titlecards

--- netflix-clone/src/components/TitleCards/TitleCards.jsx
+++ netflix-clone/src/components/TitleCards/TitleCards.jsx
@@ -3,7 +3,6 @@ import "./TitleCards.css";
 import cards_data from "../../assets/cards/Cards_data";
 
 const TitleCards = ({ title, category }) => {
-
   const cardsRef = useRef();
 
   const [apiData, setApiData] = useState([]);
@@ -23,14 +22,15 @@ const TitleCards = ({ title, category }) => {
   };
 
   useEffect(() => {
-
     fetch(
-      "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
+      `https://api.themoviedb.org/3/movie/${
+        category ? category : "now_playing"
+      }?language=en-US&page=1`,
       options
     )
       .then((res) => res.json())
       .then((res) => setApiData(res.results))
-      .catch(err => console.error(err));
+      .catch((err) => console.error(err));
 
     cardsRef.current.addEventListener("wheel", handleWheel);
   }, []);
