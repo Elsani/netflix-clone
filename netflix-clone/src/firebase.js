aac04a2b50b37ba73a998ea6c482ff9cc30d6a67
Elsani <elsan.memeti@hotmail.com>
2024-12-12 21:22:25 +0100

    firebase-database

---
+++ netflix-clone/src/firebase.js
@@ -0,0 +1,51 @@
+import { initializeApp } from "firebase/app";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { addDoc, getFirestore } from "firebase/firestore";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCJn4sLQvbj-ZAQWcmDSrxc41K04gCQ7NU",
+  authDomain: "netflixx-64a54.firebaseapp.com",
+  projectId: "netflixx-64a54",
+  storageBucket: "netflixx-64a54.firebasestorage.app",
+  messagingSenderId: "152420166432",
+  appId: "1:152420166432:web:37ad609e08e095436ac545",
+  measurementId: "G-JF393EWRFR"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app)
+
+const signup = async (name, email, password) => {
+    try {
+       const res = await createUserWithEmailAndPassword(auth,email,password)
+    const user = res.user;
+    await addDoc(collection (db, "user"), {
+        uid: user.uid,
+        name,
+        authProvider: 'local',
+        email,
+    });
+    } catch (error) {
+        console.log(error);
+        alert(error);
+    }
+}
+
+const login = async () => {
+    try {
+
+       await signInWithEmailAndPassword(auth,email,password)
+
+    } catch(error) {
+        console.log(error);
+        alert(error);
+    }
+
+}
+
+const logout = () => {
+    signOut(auth);
+}
+export{auth, db, login, signup, logout};
